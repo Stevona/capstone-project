c03e8b9c887703c2512f0af32ca6fa2947d592de
Daniel Kotlinski <dan61160@tjx.com>
2022-03-21 16:01:49 -0400

    customer routes

--- routes/customers.js
+++ routes/customers.js
@@ -2,9 +2,14 @@ var express = require('express');
 var router = express.Router();
 const { Customers } = require('../orm/tracking-model')
 
-/* GET users listing. */
-router.get('/api/customers', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET customers listing. */
+router.get('/api/customers', async(req, res) =>{
+  try{
+    let customers = await Customers.findAll();
+    res.json(customers);
+  } catch (error){
+    res.status(500).send('Customer fetching failed');
+  }
 });
 
 module.exports = router;
