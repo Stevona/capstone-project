f0a5d39b9b9055ab670dee39fe2595dc7d64b4ce
Daniel Kotlinski <dan61160@tjx.com>
2022-03-21 15:17:06 -0400

    updated customers and index API startpoints

--- routes/customers.js
+++ routes/customers.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const { Customers } = require('../orm/tracking-model')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/api/customers', function(req, res, next) {
   res.send('respond with a resource');
 });
 
