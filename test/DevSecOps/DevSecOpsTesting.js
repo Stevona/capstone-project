a72b335fc6fa755c9e99c8cb6167155abb9172d3
Abdulahad Qureshi <abd01088@tjx.com>
2022-03-22 15:19:14 -0400

    added a single file to each team under test to start

--- test/DevSecOps/DevSecOpsTesting.js
+++ test/DevSecOps/DevSecOpsTesting.js
@@ -3,6 +3,7 @@ var describe = mocha.describe
 var it = mocha.it
 var assert = require('chai').assert
 
+//below is a sample test case
 describe('sample test cases for DevSecOps team', () => {
 
     it("passing test case", () => {
