df60ec9324a3dc93484b7cf8b7418ed134bcaf38
Mary <matrat3141@gmail.com>
2022-03-22 14:25:08 -0400

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template id="navBar">
    <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Company Name</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -35,7 +35,7 @@
       
           <a class="nav-link active" aria-current="page" href="/">Home</a>
       
-        <button class="nav-button" href="#"> Log out</button>
+        <!-- <button class="nav-button" href="#"> Log out</button> -->
       
     </div>
   </div>
