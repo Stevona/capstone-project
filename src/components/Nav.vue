0caac503f976acd0165d9cff7992ae9523cc388f
Mary <matrat3141@gmail.com>
2022-03-22 12:20:42 -0400

    Update Nav.vue

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -8,7 +8,7 @@
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
         <b-nav-item-dropdown text="Customer" right>
-          <b-dropdown-item href="#">Add</b-dropdown-item> 
+          <b-dropdown-item href="">Add</b-dropdown-item> 
           <b-dropdown-item href="#">Edit</b-dropdown-item>
         </b-nav-item-dropdown>
         
@@ -20,10 +20,7 @@
 
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
-        <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-        </b-nav-form>
+        <b-link href="/">Home</b-link>
 
         
 
