b06ebd30466ff2ae09e69043031a5400756c152d
stevona <stevenportillo19@hotmail.com>
2022-03-22 10:34:55 -0400

    Added more of the File Structure and Bootstrap
    was able to edit routes and bootstrap for team to be able to start going
    in and attacking pages

---
+++ src/views/EditOrder.vue
@@ -0,0 +1,26 @@
+<template id="editOrder">
+  <b-alert show variant="danger">Page In progress</b-alert>
+  <div class="hello">
+    <h1>{{message}}</h1>
+  </div>
+</template>
+
+<script src="../js/editOrder.js"></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
