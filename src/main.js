49eab015c464f15334495d4fbd5b628684b3b86b
stevona <stevenportillo19@hotmail.com>
2022-03-22 14:01:28 -0400

    Added Component Connection for nav bar to be gloabl for all

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/'
+import NavBar from './components/NavBar.vue';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 
 const app = createApp(App)
 app.use(router)
+app.component('NavBar', NavBar)
 app.mount('#app')
