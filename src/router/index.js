5b07fe56b52fc142d43bb62cd90c8503840d41a7
Mary <matrat3141@gmail.com>
2022-03-22 14:59:26 -0400

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,7 @@ const routes = [
     component: dashboard
   },
   {
-    path: '/manageCurstomers',
+    path: '/manageCustomers',
     name: 'ManageCustomers',
     component: manageCurstomers
   },
