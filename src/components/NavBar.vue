a95c4e328337fede73d11ae8436a7c439235628a
RahulWhigTJX <rahul_whig@tjxeurope.com>
2022-03-23 11:07:04 +0000

    manage customers

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,44 +1,71 @@
 <template id="navBar">
-   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/"> 
-     <img width=150 alt="alt message" class="your-logo-css-class"
-          src="@/assets/stonecap.png">
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Customers
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="/addCustomers">Add</a></li>
-            <li><a class="dropdown-item" href="/manageCustomers">Edit</a></li>
-          </ul>
-        </li>
-       
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Orders
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="/addOrders">Add</a></li>
-            <li><a class="dropdown-item" href="/manageOrders">Edit</a></li>
-          </ul>
-        </li>
-        <li><a class="nav-link active" aria-current="page" href="/">Home</a></li>
-      </ul>
-      <!-- <form class="d-flex">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/">
+        <img
+          width="150"
+          alt="alt message"
+          class="your-logo-css-class"
+          src="@/assets/stonecap.png"
+        />
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Customers
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="/addCustomer">Add</a></li>
+              <li><a class="dropdown-item" href="/manageCustomers">Edit</a></li>
+            </ul>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Orders
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="/addOrders">Add</a></li>
+              <li><a class="dropdown-item" href="/manageOrders">Edit</a></li>
+            </ul>
+          </li>
+          <li>
+            <a class="nav-link active" aria-current="page" href="/">Home</a>
+          </li>
+        </ul>
+        <!-- <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form> -->
-      
+
         <!-- <button class="nav-button" href="#"> Log out</button> -->
-      
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
 </template>
