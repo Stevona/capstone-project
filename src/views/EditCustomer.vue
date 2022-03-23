a3bff851f3f215e7dd7043670c0e4752fd771ec8
Mary <55006288+mwishart@users.noreply.github.com>
2022-03-23 10:10:27 -0400

    Update EditCustomer.vue

--- src/views/EditCustomer.vue
+++ src/views/EditCustomer.vue
@@ -39,7 +39,7 @@
   </div>
   <div class="col">
       <div class="form-group">
-        <label for="city">State/Province</label>
+        <label for="state">State/Province</label>
         <input type="state" class="form-control" id="state">
     </div>
   </div>
