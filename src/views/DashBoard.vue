61d5d9a05943c716e9b109ef31a0b050189e24e0
Jun Hao Chia <45766347+Chiaration@users.noreply.github.com>
2022-03-23 14:53:13 +0000

    Editing Dashboard

--- src/views/DashBoard.vue
+++ src/views/DashBoard.vue
@@ -1,8 +1,27 @@
 <template id="DashBoard">
   <NavBar />
-  <div class="hello">
-    <h1>{{message}}</h1>
-  </div>
+    <div class="dashboard-main">
+      <h1>{{name}}</h1>
+      <div class="container">
+        <div class="row justify-content-md-center">
+          <div class="col-md-auto">
+            <h3>Recent Orders</h3>
+          </div>
+          <div class="col-md-auto ">
+            <h3>Quick Links</h3>
+              <button type="button" class="ql-btn col-md-auto">Create Customer</button>
+              <button type="button" class="ql-btn col-md-auto">Manage Customer</button>
+              <button type="button" class="ql-btn col-md-auto">Create Order</button>
+              <button type="button" class="ql-btn col-md-auto">Create Customer</button>
+              <button type="button" class="ql-btn col-md-auto">Manage Customer</button>
+              <button type="button" class="ql-btn col-md-auto">Create Order</button>
+              <button type="button" class="ql-btn col-md-auto">Create Customer</button>
+              <button type="button" class="ql-btn col-md-auto">Manage Customer</button>
+              <button type="button" class="ql-btn col-md-auto">Create Order</button>
+          </div>
+        </div>
+      </div>
+    </div>
   <FooterBar />
 </template>
 
@@ -10,18 +29,35 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.ql-btn {
+  border: 2px solid #79091c;
+  background-color: #9b0c23;
+  height:125px;
+  width:125px;
+  color: black;
+  cursor:pointer;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.ql-btn:hover {
+  background-color: #a0273b;
+  color:white;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+h1 {
+  font-weight: bolder;
+  color: black;
+  text-shadow: 2px 2px #fdfdfd;
 }
-a {
-  color: #42b983;
+.dashboard-main {
+  background-image: url("../assets/dashboard-background.png");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  /* background-color: #d3ccbd; */
+  height: 100%;
+  width: 100%;
+  position: fixed;
+
+  padding: 1vh;
 }
 </style>
