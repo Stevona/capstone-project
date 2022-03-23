eb973629199cb7fc167cf1496e3b282f15636b5a
RahulWhigTJX <rahul_whig@tjxeurope.com>
2022-03-23 13:21:11 +0000

    ManageOrders & ManageCustomers files

--- src/views/ManageCustomers.vue
+++ src/views/ManageCustomers.vue
@@ -1,9 +1,81 @@
 <template id="manageCustomers">
   <NavBar />
   <div class="hello">
-    <h1>{{message}}</h1>
+    <h1>{{ message }}</h1>
   </div>
   <FooterBar />
+
+  <div class="input-group rounded container mt-5">
+    <input
+      type="search"
+      class="form-control rounded"
+      placeholder="Search"
+      aria-label="Search"
+      aria-describedby="search-addon"
+    />
+    <span class="input-group-text border-0" id="search-addon">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        class="bi bi-search"
+        viewBox="0 0 16 16"
+      >
+        <path
+          d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
+        />
+      </svg>
+    </span>
+  </div>
+
+  <table class="table table-hover container mt-5">
+    <thead>
+      <tr>
+        <th scope="col">Name</th>
+        <th scope="col">Address</th>
+        <th scope="col">Phone</th>
+        <th scope="col">Recent Order ID</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">John Doe</th>
+        <td>14 Road St.</td>
+        <td>555444888</td>
+        <td>23534</td>
+        <td>
+          <button type="button" class="btn btn-outline-primary"></button>
+        </td>
+      </tr>
+      <tr>
+        <th scope="row">John Doe</th>
+        <td>14 Road St.</td>
+        <td>555444888</td>
+        <td>23534</td>
+        <td>
+          <button type="button" class="btn btn-outline-primary"></button>
+        </td>
+      </tr>
+      <tr>
+        <th scope="row">John Doe</th>
+        <td>14 Road St.</td>
+        <td>555444888</td>
+        <td>23534</td>
+        <td>
+          <button type="button" class="btn btn-outline-primary"></button>
+        </td>
+      </tr>
+      <th scope="row"></th>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td>
+        <button type="button" class="btn btn-outline-primary"></button>
+      </td>
+    </tbody>
+  </table>
+  <button type="button" class="btn btn-primary btn-sm">New Customer</button>
 </template>
 
 <script src="../js/manageCustomers.js"></script>
