9267913d2ed947ebb481d516981a2e4cd9aa4a53
Bri Fahrenkopf <bri@Bris-MacBook-Pro.local>
2022-03-23 08:40:41 -0400

    remove allow null from customer notes

--- orm/Customer.js
+++ orm/Customer.js
@@ -23,7 +23,7 @@ const Customer = connection.define('customer', {
     city: { type: DataTypes.STRING, allowNull: false},
     region: { type: DataTypes.STRING, allowNull: false},
     zip: { type: DataTypes.STRING, allowNull: false},
-    customerNotes: { type: DataTypes.STRING, allowNull: true},
+    customerNotes: { type: DataTypes.STRING},
     customerId: { type: DataTypes.INTEGER, allowNull: false, autoIncrement:true, primaryKey: true}
   });
   
