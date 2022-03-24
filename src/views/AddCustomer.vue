6bb521e976f59d5f979aa52a8cc2cd385df5d704
Chris Gritter <chr13546@tjx.com>
2022-03-24 11:28:37 -0400

    Changed form for add/edit customer

--- src/views/AddCustomer.vue
+++ src/views/AddCustomer.vue
@@ -4,81 +4,92 @@
     <h1>{{ message }}</h1>
   </div>
   <FooterBar />
-  
+
   <form class="container mb-4">
-  <div class="row mb-3">
+    <div class="row mb-3">
       <div class="col">
         <div class="form-group">
-        <label for="FirstName">First Name</label>
-        <input type="firstName" class="form-control" id="firstName" aria-describedby="emailHelp">
+          <label for="FirstName">First Name</label>
+          <input
+            type="firstName"
+            class="form-control"
+            id="firstName"
+            aria-describedby="emailHelp"
+          />
+        </div>
+      </div>
+      <div class="col">
+        <div class="form-group">
+          <label for="middleName">Middle Name</label>
+          <input type="middleName" class="form-control" id="middleName" />
+        </div>
+      </div>
+      <div class="col">
+        <div class="form-group">
+          <label for="lastName">Last Name</label>
+          <input type="lastName" class="form-control" id="lastName" />
+        </div>
+      </div>
+      <div class="col">
+        <div class="form-group">
+          <label for="phone">Phone Number</label>
+          <input type="phone" class="form-control" id="phone" />
+        </div>
+      </div>
+      <div class="col">
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input type="email" class="form-control" id="email" />
+        </div>
       </div>
     </div>
-    <div class="col">
-      <div class="form-group">
-        <label for="middleName">Middle Name</label>
-        <input type="middleName" class="form-control" id="middleName">
-        </div>
-  </div>
-  <div class ="col">
-      <div class="form-group">
-        <label for="lastName">Last Name</label>
-        <input type="lastName" class="form-control" id="lastName">
-        </div>
-  </div>
-  </div>
-  <div class="form-group mb-3">
-    <label for="address">Address</label>
-    <input type="address" class="form-control" id="address">
-  </div>
-  <div class="row mb-3">
-    <div class="col">
-      <div class="form-group">
-        <label for="city">City</label>
-        <input type="city" class="form-control" id="city">
+    <div class="form-group mb-3">
+      <label for="address">Address</label>
+      <input type="address" class="form-control" id="address" />
     </div>
-  </div>
-  <div class="col">
-      <div class="form-group">
-        <label for="state">State/Province</label>
-        <input type="state" class="form-control" id="state">
+    <div class="row mb-3">
+      <div class="col">
+        <div class="form-group">
+          <label for="city">City</label>
+          <input type="city" class="form-control" id="city" />
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="form-group">
+          <label for="region">Region/Province/State</label>
+          <input type="region" class="form-control" id="region" />
+        </div>
+      </div>
+      <div class="col">
+        <div class="col-auto">
+          <label for="state">Country</label>
+          <select class="form-select" id="inlineFormCustomSelect">
+            <option value="N/A">N/A</option>
+            <option value="Canada">Canada</option>
+            <option value="United Kingdom">United Kingdom</option>
+            <option value="United States">United States</option>
+          </select>
+        </div>
+      </div>
+      <div class="col">
+        <div class="form-group">
+          <label for="zip">Zip code</label>
+          <input type="zip" class="form-control" id="zip" />
+        </div>
+      </div>
     </div>
-  </div>
-  <div class="col">
+    <div class="form-group">
+      <label for="notes">Customer Notes</label>
+      <input type="notes" class="form-control" id="notes" />
+    </div>
+  </form>
+  <form class="row g-10 d-flex justify-content-center">
     <div class="col-auto">
-    <label for="state">Country</label>
-      <select class="form-select" id="inlineFormCustomSelect">
-        <option value="1">N/A</option>
-        <option value="1">Canada</option>
-        <option value="2">United Kingdom</option>
-        <option value="3">United States</option>
-      </select>
+      <button type="submit" class="btn btn-primary">Submit</button>
     </div>
-  </div>
-  <div class="col">
-      <div class="form-group">
-        <label for="zip">Zip code</label>
-        <input type="zip" class="form-control" id="zip">
-    </div>
-  </div>
-  </div>
-  <div class="form-group">
-        <label for="notes">Customer Notes</label>
-        <input type="notes" class="form-control" id="notes">
-    </div>
-  
-</form>
-<form class="row g-10 d-flex justify-content-center">
-  <div class="col-auto">
-    <button type="draft" class="btn btn-primary mb-1">Save as Draft</button>
-  </div>
-  <div class="col-auto">
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </div>
-</form>
+  </form>
 </template>
-
-
-
 
 <script src="../js/addCustomer.js"></script>
 
