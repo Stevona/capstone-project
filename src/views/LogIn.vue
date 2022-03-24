678c2aca98c75dbbefa41a689d5806d78c0bcdca
Mary <55006288+mwishart@users.noreply.github.com>
2022-03-24 11:58:13 -0400

    Update LogIn.vue
    Text change

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -10,7 +10,7 @@
     <form class="container">
      <div class="form-group">
       <label for="username">Username</label>
-       <input type="email" class="form-control" id="username" aria-describedby="emailHelp" placeholder="Enter username">
+       <input type="email" class="form-control" id="username" aria-describedby="emailHelp" placeholder="Username">
      </div>
      <div class="form-group">
       <label for="exampleInputPassword1">Password</label>
