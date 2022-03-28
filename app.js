dd9925c1d7e964f34db4aae6e7c61653d87cf405
Jun Hao Chia <45766347+Chiaration@users.noreply.github.com>
2022-03-28 11:59:56 +0100

    Fixed path bug

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/customers', customersRouter);
 app.use('/api/products', productsRouter);
