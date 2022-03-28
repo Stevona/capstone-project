82a10fdf8fc3d95bbb86f0398f4d4fd1bf183d1c
Katrina Wallace <kat13180@tjx.com>
2022-03-28 14:55:02 -0400

    validation tests added

--- test/routes/customers.test.js
+++ test/routes/customers.test.js
@@ -35,12 +35,13 @@ describe('Customer', () => {
     });
 
     describe('/POST customer', () => {
-        it('it should not POST a customer without email field', (done) => {
+        it('it should not POST a customer if email validation fails', (done) => {
             let testCustomer = {
                 firstName: "Brianna",
                 middleName: "Lynn",
                 lastName: "Fahrenkopf",
                 phone: "6367512114",
+                email: "testtjx.com",
                 address: "116 Martin St",
                 city: "Lowell",
                 region: "Massachusetts",
@@ -54,6 +55,248 @@ describe('Customer', () => {
                     res.should.have.status(400);
                     res.body.should.be.a('object');
                     res.body.errors[0].msg.should.be.eql('Must be a valid email');
+                done();
+              });
+        });
+
+        it('it should not POST a customer if email sanitization fails', (done) => {
+            let testCustomer = {
+                firstName: "Brianna",
+                middleName: "Lynn",
+                lastName: "Fahrenkopf",
+                phone: "6367512114",
+                email: "tEsT+blah@gmail.com",
+                address: "116 Martin St",
+                city: "Lowell",
+                region: "Massachusetts",
+                zip: "01854",
+                country: 'US'
+            }
+          chai.request(server)
+              .post('/api/customers')
+              .send(testCustomer)
+              .end((err, res) => {
+                    res.should.have.status(201);
+                    res.body.should.be.a('object');
+                    res.body.email.should.be.eql('test@gmail.com');
+                done();
+              });
+        });
+
+        it('it should not POST a customer if firstName validation fails', (done) => {
+            let testCustomer = {
+                firstName: "Br444ianna",
+                middleName: "Lynn",
+                lastName: "Fahrenkopf",
+                phone: "6367512114",
+                email: "test@tjx.com",
+                address: "116 Martin St",
+                city: "Lowell",
+                region: "Massachusetts",
+                zip: "01854",
+                country: 'US'
+            }
+          chai.request(server)
+              .post('/api/customers')
+              .send(testCustomer)
+              .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.a('object');
+                    res.body.errors[0].msg.should.be.eql('First name must be alphabetical');
+                done();
+              });
+        });
+    
+        it('it should not POST a customer if middleName validation fails', (done) => {
+            let testCustomer = {
+                firstName: "Brianna",
+                middleName: "L444ynn",
+                lastName: "Fahrenkopf",
+                phone: "6367512114",
+                email: "test@tjx.com",
+                address: "116 Martin St",
+                city: "Lowell",
+                region: "Massachusetts",
+                zip: "01854",
+                country: 'US'
+            }
+          chai.request(server)
+              .post('/api/customers')
+              .send(testCustomer)
+              .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.a('object');
+                    res.body.errors[0].msg.should.be.eql('Middle name must be alphabetical');
+                done();
+              });
+        });
+
+        it('it should not POST a customer if lastName validation fails', (done) => {
+            let testCustomer = {
+                firstName: "Brianna",
+                middleName: "Lynn",
+                lastName: "Fah4444renkopf",
+                phone: "6367512114",
+                email: "test@tjx.com",
+                address: "116 Martin St",
+                city: "Lowell",
+                region: "Massachusetts",
+                zip: "01854",
+                country: 'US'
+            }
+          chai.request(server)
+              .post('/api/customers')
+              .send(testCustomer)
+              .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.a('object');
+                    res.body.errors[0].msg.should.be.eql('Last name must be alphabetical');
+                done();
+              });
+        });
+
+        it('it should not POST a customer if country validation fails', (done) => {
+            let testCustomer = {
+                firstName: "Brianna",
+                middleName: "Lynn",
+                lastName: "Fahrenkopf",
+                phone: "6367512114",
+                email: "test@tjx.com",
+                address: "116 Martin St",
+                city: "Lowell",
+                region: "Massachusetts",
+                zip: "01854",
+                country: 'US1'
+            }
+          chai.request(server)
+              .post('/api/customers')
+              .send(testCustomer)
+              .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.a('object');
+                    res.body.errors[0].msg.should.be.eql('Country must be alphabetical');
+                done();
+              });
+        });
+
+        it('it should not POST a customer if address validation fails', (done) => {
+            let testCustomer = {
+                firstName: "Brianna",
+                middleName: "Lynn",
+                lastName: "Fahrenkopf",
+                phone: "6367512114",
+                email: "test@tjx.com",
+                address: "116 Martin St!@",
+                city: "Lowell",
+                region: "Massachusetts",
+                zip: "01854",
+                country: 'US'
+            }
+          chai.request(server)
+              .post('/api/customers')
+              .send(testCustomer)
+              .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.a('object');
+                    res.body.errors[0].msg.should.be.eql('Address must be alphanumeric');
+                done();
+              });
+        });
+        
+        it('it should not POST a customer if city validation fails', (done) => {
+            let testCustomer = {
+                firstName: "Brianna",
+                middleName: "Lynn",
+                lastName: "Fahrenkopf",
+                phone: "6367512114",
+                email: "test@tjx.com",
+                address: "116 Martin St",
+                city: "Lowell!!",
+                region: "Massachusetts",
+                zip: "01854",
+                country: 'US'
+            }
+          chai.request(server)
+              .post('/api/customers')
+              .send(testCustomer)
+              .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.a('object');
+                    res.body.errors[0].msg.should.be.eql('City must be alphanumeric');
+                done();
+              });
+        });
+
+
+        it('it should not POST a customer if region validation fails', (done) => {
+            let testCustomer = {
+                firstName: "Brianna",
+                middleName: "Lynn",
+                lastName: "Fahrenkopf",
+                phone: "6367512114",
+                email: "test@tjx.com",
+                address: "116 Martin St",
+                city: "Lowell",
+                region: "Massachusetts!!",
+                zip: "01854",
+                country: 'US'
+            }
+          chai.request(server)
+              .post('/api/customers')
+              .send(testCustomer)
+              .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.a('object');
+                    res.body.errors[0].msg.should.be.eql('Region must be alphanumeric');
+                done();
+              });
+        });
+
+
+        it('it should not POST a customer if address validation fails', (done) => {
+            let testCustomer = {
+                firstName: "Brianna",
+                middleName: "Lynn",
+                lastName: "Fahrenkopf",
+                phone: "6367512114",
+                email: "test@tjx.com",
+                address: "116 Martin St!@",
+                city: "Lowell",
+                region: "Massachusetts",
+                zip: "01854",
+                country: 'US'
+            }
+          chai.request(server)
+              .post('/api/customers')
+              .send(testCustomer)
+              .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.a('object');
+                    res.body.errors[0].msg.should.be.eql('Address must be alphanumeric');
+                done();
+              });
+        });
+
+        it('it should not POST a customer if phone validation fails', (done) => {
+            let testCustomer = {
+                firstName: "Brianna",
+                middleName: "Lynn",
+                lastName: "Fahrenkopf",
+                phone: "6367512114ab",
+                email: "test@tjx.com",
+                address: "116 Martin St",
+                city: "Lowell",
+                region: "Massachusetts",
+                zip: "01854",
+                country: 'US'
+            }
+          chai.request(server)
+              .post('/api/customers')
+              .send(testCustomer)
+              .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.a('object');
+                    res.body.errors[0].msg.should.be.eql('Must be a valid phone number');
                 done();
               });
         });
