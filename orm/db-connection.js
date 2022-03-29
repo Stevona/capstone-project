c50466f99f69f8399f2d423f1147cab11abb77ba
Daniel Kotlinski <dan61160@tjx.com>
2022-03-29 14:16:41 -0400

    updates for devsecops

--- orm/db-connection.js
+++ orm/db-connection.js
@@ -3,23 +3,23 @@ const { Sequelize } = require('sequelize');
 
 
 
-const connString = process.env.MYSQLCONNSTR_officialstonecapDatabase;
+// const connString = process.env.MYSQLCONNSTR_officialstonecapDatabase;
 
-// protocol://user:password@server:port/databaseName
-const connection = new Sequelize(
-  connString,
-  {
-    define:{ timestamps: false }
-  }
-);
-
-// const connection = new Sequelize(process.env.DATABASE_NAME, process.env.USER_NAME, process.env.PASSWORD, {
-//   host: process.env.HOST,
-//   dialect: 'mysql',
-//   define : {
-//     timestamps : false
+// // protocol://user:password@server:port/databaseName
+// const connection = new Sequelize(
+//   connString,
+//   {
+//     define:{ timestamps: false }
 //   }
-// })
+// );
+
+const connection = new Sequelize(process.env.DATABASE_NAME, process.env.USER_NAME, process.env.PASSWORD, {
+  host: process.env.HOST,
+  dialect: 'mysql',
+  define : {
+    timestamps : false
+  }
+})
 
 
 module.exports = connection;
