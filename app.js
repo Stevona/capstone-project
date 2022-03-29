9c082e53bc70a2a807f4b09e6cb2558e8fc4073c
BaltejToorTJX <baltej_toor@tjxcanada.ca>
2022-03-29 07:30:01 -0400

    Add User model and initial pw check

--- app.js
+++ app.js
@@ -3,12 +3,34 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var history = require('connect-history-api-fallback');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const { DataTypes } = require('sequelize');
+const connection = require('./orm/db-connection');
 
 // Commented old code to test solution - Remove this if solution works on cloud
 // var indexRouter = require('./routes/index');
 
+const User = connection.define('User', {
+    loginId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    userName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+
 var customersRouter = require('./routes/customers');
 var productsRouter = require('./routes/products');
+const bcryptjs = require('bcryptjs');
 
 var app = express();
 
@@ -35,6 +57,38 @@ app.use(history({
 app.use(express.static(path.join(__dirname, 'dist')));
 app.get('/', function (req,res) {
     res.sendFile(path + "index.html");
+});
+
+app.post('/login', async(req, res) => {
+    const name = req.body.name;
+    const password = req.body.password;
+
+    let isAuth = false;
+
+    try{
+        let user = await User.findOne({
+            where: {
+                userName: name
+            }
+        });
+        if (user) {
+            console.log(`User ${user.userName} found`);
+            isAuth = await bcrypt.compare(password, user.password);
+            if (isAuth) {
+                console.log('User authenticated');
+                res.status(200).send('User authenticated');
+            } else {
+                console.log('User is not authenticated');
+                res.status(200).send('User is not authenticated');
+            }
+        } else {
+            console.log(`User not found`);
+            res.status(404).send('User not found');
+        }
+    } catch(error){
+        console.log(error);
+        res.status(500).send('Failure checking user credentials');
+    }
 });
 
 module.exports = app;
