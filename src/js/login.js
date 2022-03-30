2f7edeca63bd5bf2b580fdc60d401367c40d8a44
stevona <stevenportillo19@hotmail.com>
2022-03-30 11:56:55 -0400

    Implemented Login

--- src/js/login.js
+++ src/js/login.js
@@ -6,10 +6,34 @@ export default defineComponent({
   data() {
     return {
       message: "",
+      username: '',
+      password: '',
+      loading: false,
     };
   },
   methods: {
-    
+    async login () {
+      this.loading = true
+      try {
+        const response = await fetch('/login', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            name: this.username,
+            password: this.password,
+          })
+        })
+        let tempUser = await response.json()
+        localStorage.setItem('user',tempUser.token)
+        window.location.href = '/';
+      } catch(error) {
+        console.log(error)
+      }
+      this.loading = false;
+    },
   },
   mounted() {
     this.message = "Login";
