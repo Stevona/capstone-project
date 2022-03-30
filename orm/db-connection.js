25be593555c14268fce8772022b60da7ba04c6cf
stevona <stevenportillo19@hotmail.com>
2022-03-30 10:17:26 -0400

    database stuff was acceidently changed

--- orm/db-connection.js
+++ orm/db-connection.js
@@ -1,15 +1,23 @@
 //use sequelize to access mysql database
 const { Sequelize } = require('sequelize');
 
-const config = require('./db-config');
+/* const connString = process.env.MYSQLCONNSTR_officialstonecapDatabase;
 
 // protocol://user:password@server:port/databaseName
 const connection = new Sequelize(
-
-  `mysql://${config.userName}:${config.password}@localhost:3306/${config.database}`,
+  connString,
   {
-    define:{timestamps: false}
+    define:{ timestamps: false }
   }
-);
+); */
+
+const connection = new Sequelize(process.env.DATABASE_NAME, process.env.USER_NAME, process.env.PASSWORD, {
+  host: process.env.HOST,
+  dialect: 'mysql',
+  define : {
+    timestamps : false
+  }
+})
+
 
 module.exports = connection;
