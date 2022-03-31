f7d7f72b2e4cf3838bf2f50a7c4a86fd3875aa9c
BaltejToorTJX <baltej_toor@tjxcanada.ca>
2022-03-31 10:20:41 -0400

    Customer and Order associations for GET calls

--- orm/tracking-model.js
+++ orm/tracking-model.js
@@ -5,12 +5,21 @@ const Order = require('./Order');
 const OrderStatusCode = require('./OrderStatusCode');
 const OrderProduct = require('./OrderProduct');
 
-Product.belongsToMany(Order, {through:OrderProduct});
-OrderProduct.hasMany(Product, {foreignKey: 'productId'});
-Order.belongsToMany(Product,{through: OrderProduct});
-OrderProduct.hasMany(Order, {foreignKey: 'orderId'});
+Product.belongsToMany(Order, {
+    through: { model: OrderProduct },
+    foreignKey: 'productId'
+});
+//OrderProduct.hasMany(Product, {foreignKey: 'productId'});
+
+Order.belongsToMany(Product, {
+    through: { model: OrderProduct },
+    foreignKey: 'orderId'
+});
+//OrderProduct.hasMany(Order, {foreignKey: 'orderId'});
+
 Order.belongsTo(Customer, {foreignKey:'customerId'});
 Customer.hasMany(Order, {foreignKey: 'customerId'});
+
 OrderStatusCode.belongsTo(Order,{foreignKey: 'orderStatusCodeId'});
 Order.hasOne(OrderStatusCode, {foreignKey: 'orderStatusCodeId'});
 
@@ -24,4 +33,4 @@ connection
         console.error("Unable to connect to the database: ", error);
     });
 
-module.exports = { Customer, Product, Order};
+module.exports = { Customer, Product, Order, OrderStatusCode, OrderProduct };
